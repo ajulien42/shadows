3f76e3c99b67fe359b6459efd3027268a735345e
antoine <antoinejulien33@gmail.com>
2016-09-22 17:49:19 +0200

    eol caca

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+    public GameObject holderRotX;
+    public GameObject holderRotY;
+    public GameObject holderPos;
+    public GameObject model;
+
+    private Quaternion startRotX;
+    private Quaternion startRotY;
+    private Vector3 startPos;
+
+    public bool isGameOn;
+    public bool isRotX;
+    public bool isRotY;
+    public bool isPos;
+
+    // Use this for initialization
+    void Start () {
+        startRotX = holderRotX.transform.rotation;
+        startRotY = holderRotY.transform.rotation;
+        startPos = holderPos.transform.position;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameRotX();
+	}
+
+    private void gameRotX() {
+        if (isGameOn == false)
+            return;
+        else {
+           // holderRot.transform.rotation = Quaternion.eulerAngles = new Vector3(0, 30, 0);
+        }
+    }
+
+    public void setIsGame(bool bb) {
+        isGameOn = bb;
+    }
+}
---
+++ Assets/Scripts/MainGestionCamera.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainGestionCamera : MonoBehaviour
+{
+    //machine à gaz droit devant
+    public GameObject lvl1;
+    public GameObject lvl2;
+    public GameObject lvl3;
+    public GameObject lvl4;
+
+    public GameObject Ui;
+
+    private Vector3 MainPos;
+    private Vector3 lvl1Pos;
+    private Vector3 lvl2Pos;
+    private Vector3 lvl3Pos;
+    private Vector3 lvl4Pos;
+
+    private Quaternion MainRot;
+    private Quaternion lvl1Rot;
+    private Quaternion lvl2Rot;
+    private Quaternion lvl3Rot;
+    private Quaternion lvl4Rot;
+
+
+
+    public int move; //initial cam is 42
+    float t;
+
+    // Use this for initialization
+    void Start()
+    {
+        MainPos = Camera.main.transform.position;
+
+        lvl1Pos = lvl1.transform.position;
+        lvl2Pos = lvl2.transform.position;
+        lvl3Pos = lvl3.transform.position;
+        lvl4Pos = lvl4.transform.position;
+
+        MainRot = Camera.main.transform.rotation;
+        lvl1Rot = lvl1.transform.rotation;
+        lvl2Rot = lvl2.transform.rotation;
+        lvl3Rot = lvl3.transform.rotation;
+        lvl4Rot = lvl4.transform.rotation;
+        t = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCam(this.move);
+    }
+
+    void MoveCam(int i)
+    {
+        if (i == 0)
+            return;
+        if (i == 1) {
+            Ui.SetActive(false);
+            mySlerp(lvl1Pos, lvl1Rot);
+        }
+        if (i == 2) {
+            Ui.SetActive(false);
+            mySlerp(lvl2Pos, lvl2Rot);
+        }
+        if (i == 3) {
+            Ui.SetActive(false);
+            mySlerp(lvl3Pos, lvl3Rot);
+        }
+        if (i == 4) {
+            Ui.SetActive(false);
+            mySlerp(lvl4Pos, lvl4Rot);
+        }
+        if (i == 42) {
+            Ui.SetActive(true);
+            mySlerp(MainPos, MainRot);
+        }
+    }
+    private void mySlerp(Vector3 destV, Quaternion destR) {
+        t += Time.deltaTime / 20;
+
+        Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, destV, t);
+        Camera.main.transform.rotation = Quaternion.Slerp(Camera.main.transform.rotation, destR, t);
+
+        if (t >= 0.1f)
+        {
+            this.move = 0;
+            t = 0;
+            Camera.main.transform.position = destV;
+            Camera.main.transform.rotation = destR;
+        }
+    }
+
+    public void setMoveTo(int i) {
+        this.move = i;
+    }
+}
