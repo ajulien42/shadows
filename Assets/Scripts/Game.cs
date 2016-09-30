dae111cbc99419c0199d4482f1b739c12a57b4ef
antoine <antoinejulien33@gmail.com>
2016-09-30 18:50:22 +0200

    toto

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,16 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Game : MonoBehaviour {
+public class Game : MonoBehaviour
+{
 
     public GameObject holderRotX;
     public GameObject holderRotY;
     public GameObject holderPos;
-    public GameObject model;
+    public GameObject wall;
 
     private Quaternion startRotX;
     private Quaternion startRotY;
     private Vector3 startPos;
+    private Renderer rend;
+    private Color color;
+    private float diffAnglesX;
+    private float diffAnglesY;
 
     public bool isGameOn;
     public bool isRotX;
@@ -18,26 +23,54 @@ public class Game : MonoBehaviour {
     public bool isPos;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         startRotX = holderRotX.transform.rotation;
         startRotY = holderRotY.transform.rotation;
-        startPos = holderPos.transform.position;	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        gameRotX();
-	}
+        startPos = holderPos.transform.position;
+        
+        rend = wall.GetComponent<Renderer>();
+        
+        if (isRotX)
+            holderRotX.transform.rotation *= Quaternion.Euler(0, 0, Random.Range(50, 310));
 
-    private void gameRotX() {
-        if (isGameOn == false)
-            return;
-        else {
-           // holderRot.transform.rotation = Quaternion.eulerAngles = new Vector3(0, 30, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGameOn)
+            gameCheck();
+    }
+
+    private void gameCheck()
+    {
+        if (isRotX)
+        {
+            diffAnglesX = Quaternion.Angle(startRotX, holderRotX.transform.rotation);
+            Debug.Log(diffAnglesX);
+            color = new Color(0, 0, (180 - diffAnglesX) / 180);
+            rend.material.SetColor("_SpecColor", color);
+
         }
     }
 
-    public void setIsGame(bool bb) {
+    public void setIsGame(bool bb)
+    {
         isGameOn = bb;
+    }
+
+    void OnMouseDrag()
+    {
+        if (isGameOn == true)
+        {
+            float rotSpeed = 180;
+            if (isRotX == true)
+            {
+                float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+
+                holderRotX.transform.rotation *= Quaternion.Euler(0, 0, -rotX);
+            }//transform.RotateAround(Vector3.right, rotY);
+        }
     }
 }
